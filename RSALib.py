64e852e49791422e84d7bad6be55029114f7da33
ryandchandra <ryandchandra1@gmail.com>
2021-03-25 20:30:19 +0700

    Encrypt Decrypt Function (belum dicoba)

--- RSALib.py
+++ RSALib.py
@@ -75,107 +75,49 @@ def BlockCiphertext(ciphertext,n):
         ciphertext_block.append(int(block,16))
 
     return ciphertext_block
-
-def ModifiedKey(key):
-    # Create Modified key in Stream Cipher
-    # Input : key (string any length)
-    # Output : modified key in numbers (length >= 256)
-
-    # Change key to numbers
-    modified_key = StringToByteIntArray(key)
-
-    # Modified key if length <256
-    i = len(modified_key)
-    while (i<256):
-        if (i==1):
-            modified_key.append((2*modified_key[i-1])%256)
-        else:
-            modified_key.append((modified_key[i-1]+modified_key[i-2])%256)
-        i = len(modified_key)
-
-    return modified_key
-
-
-def PermutationEncrypt(byteintarray,block_length):
-    # Melakukan permutasi lanjutan pada byteintarray
-    # Permutasi yang dilakukan : 
-    # 1. Membagi byteintarray menjadi blok-blok dengan panjang blocklength, kemudian masing-masing blok diberikan offset sesuai urutan blok
-    #    Misal : blok 1 diberikan offset 1, blok 2 diberikan offset 2, dst
-    # 2. Setiap pasangan 2 byte diswap. Jika jumlah byte ganjil, byte terakhir dibiarkan
-    # Input : byteintarray (array of byte yang akan dipermutasi), block_length (panjang blok)
-    # Output : array of integer (byte) yang sudah diubah
     
-    modified_byteintarray = byteintarray
-    array_length = len(byteintarray)
-    
-    # Berikan offset sesuai nomor blok
-    for i in range(array_length):
-        offset = i//block_length + 1
-        modified_byteintarray[i] = (modified_byteintarray[i] + offset)%256
-
-    # Lakukan swap untuk setiap pasangan 2 byte
-    i = 0
-    while (i<array_length):
-        if (i!=array_length-1):
-            modified_byteintarray[i], modified_byteintarray[i+1] = modified_byteintarray[i+1], modified_byteintarray[i]
-            i = i + 2
-        else:
-            i = i + 1
-    
-    return modified_byteintarray
-    
-def RSAEncrypt(plaintext_byteintarray,key):
-    # Create Normal PRGA Encrypt in Stream Cipher
+def RSAEncrypt(plaintext_byteintarray,e,n,size):
+    # RSA Encrypt
     # Input :   plaintext_byteintarray (byte in array)
-    #           key (string any length)
-    # Output :  ciphertext (byte in array)
+    #           key (e,n)
+    #           block size
+    # Output :  ciphertext string (in hex)
 
-    # Generate key sesuai algoritma yang dimodifikasi
-    larik_S = ModifiedKSA(key)
-    i = 0
-    j = 0
-    ciphertext_byteintarray = []
-
-    # PRGA
-    # Catatan : Diadaptasi dari slide kuliah II4031 Kriptografi dan Koding yang berjudul "Stream Cipher" (2021) oleh Rinaldi Munir pada slide 31-32
-    for idx in range(len(plaintext_byteintarray)):
-        i = (i+1)%256
-        j = (j+larik_S[i])%256
-        larik_S[i], larik_S[j] = larik_S[j], larik_S[i]
-        temp = (larik_S[j] + larik_S[i])%256
-        keystream = larik_S[temp]
-        ciphertext_byteintarray.append(keystream^plaintext_byteintarray[idx])
-    # Akhir dari bagian yang diadaptasi
+    plaintext_blocks = BlockByteIntArray(plaintext_byteintarray,size)
+    ciphertext_blocksize = math.ceil(math.log(n,16))
     
-    # Lakukan postprocessing lanjutan untuk permutasi
-    ciphertext_byteintarray = PermutationEncrypt(ciphertext_byteintarray,len(key))
+    ciphertext_hexstr = ""
+    for block in plaintext_blocks:
+        cipher_block = (block**e)%n
+        cipher_hex = str(hex(cipher_block))[2:]
+        if (len(cipher_hex)<ciphertext_blocksize):
+            leading_zero = "0" * (ciphertext_blocksize-len(cipher_hex))
+            cipher_hex = leading_zero + cipher_hex
+            
+        ciphertext_hexstr += cipher_hex
     
-    return ciphertext_byteintarray
+    return ciphertext_hexstr
     
-def RSADecrypt(ciphertext_byteintarray,key):
-    # Create Normal PRGA Decrypt in Stream Cipher
-    # Input :   ciphertext_byteintarray (byte in array)
-    #           key (string any length)
+def RSADecrypt(ciphertext_hexstr,d,n):
+    # RSA Decrypt
+    # Input :   ciphertext_hexstr (string of hexadecimal)
+    #           key (d,n)
     # Output :  plaintext (byte in array)
 
-    # Generate key sesuai algoritma yang dimodifikasi
-    larik_S = ModifiedKSA(key)
-    i = 0
-    j = 0
-    plaintext_byteintarray = []
-
-    # Kembalikan terlebih dahulu postprocessing permutasi yang dilakukan
-    ciphertext_byteintarray = PermutationDecrypt(ciphertext_byteintarray,len(key))
-
-    # PRGA
-    # Catatan : Diadaptasi dari slide kuliah II4031 Kriptografi dan Koding yang berjudul "Stream Cipher" (2021) oleh Rinaldi Munir pada slide 33-34
-    for idx in range(len(ciphertext_byteintarray)):
-        i = (i+1)%256
-        j = (j+larik_S[i])%256
-        larik_S[i], larik_S[j] = larik_S[j], larik_S[i]
-        temp = (larik_S[j] + larik_S[i])%256
-        keystream = larik_S[temp]
-        plaintext_byteintarray.append(keystream^ciphertext_byteintarray[idx])
-    # Akhir dari bagian yang diadaptasi
+    ciphertext_blocks = BlockCiphertext(ciphertext_hexstr)
+    
+    plaintext = ""
+    for block in ciphertext_blocks:
+        plaintext_block = (int(block,16)**d)%n
+        plaintext_blockstr = str(plaintext_block)
+        if (len(plaintext_blockstr)%3!=0):
+            leading_zero = "0" * (3-len(plaintext_blockstr)%3)
+            plaintext_blockstr = leading_zero + plaintext_blockstr
             
-    return plaintext_byteintarray
+        i = 0
+        while (i<len(plaintext_blockstr)):
+            num = plaintext_blockstr[i:i+3]
+            plaintext += chr(int(num))
+            i = i + 3
+            
+    return plaintext
